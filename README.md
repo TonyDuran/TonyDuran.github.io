652f4ac2555add87cd165bdbcf3fa7170841150c
Tony Duran <tony-duran@outlook.com>
2023-12-05 20:55:26 -0600

    removing default example to better follow class

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # TonyDuran.github.io
-Welcome to "Thought bin" and portfolio site using Vue.js.
+Welcome to my "Thought bin" and portfolio site using Vue.js.
+
+I'm currently in the middle of following a Udemy Vue class. So there might be experimental changes from this class.
 
 
 Special thanks to Roland Doda and his [how-to](https://dev.to/the_one/deploy-to-github-pages-like-a-pro-with-github-actions-4hdg).
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import { Timeline } from './views/TimelineView.vue'
 
 const router = createRouter({
   history: createWebHistory("/TonyDuran.github.io/"),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/TonyDuran.github.io/',
-      name: 'Home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: 
+    // },
+    // {
+    //   path: '/TonyDuran.github.io/',
+    //   name: 'Home',
+    //   component: Timeline
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
