5b0e625db0414b696d53d74ef2a0b5ad7de2f778
Tony Duran <tony-duran@outlook.com>
2023-12-11 12:58:26 -0600

    adding typing example from course

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
+// type Period = "Today" | "This Week" | "This Month"
+const periods = ["Today", "This Week", "This Month"] as const
+
+type Period = typeof periods[number]
+
+const selectedPeriod = ref("Today");
+
+function selectPeriod(period: Period) {
+  selectedPeriod.value = period;
+}
+
 </script>
 
 <template>
-  Timeline
   <nav class="is-primary panel">
+    {{ selectedPeriod }}
     <span class="panel-tabs">
-      <a>Today</a>
-      <a>This Week</a>
-      <a>This Month</a>
+      <a v-for="period of periods" :key="period" :class="{ 'is-active': period === selectedPeriod }"
+        @click="selectPeriod(period)">
+        {{ period }}
+      </a>
     </span>
   </nav>
 </template>
