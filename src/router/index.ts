73b0a007d3031b56c9d7c5fda92574bb27b07051
Tony Duran <tony-duran@outlook.com>
2024-06-19 16:30:59 -0500

    adding tweaks to limit routing while I figure out structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import NewPost from '@/views/NewPost.vue'
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
+const routes = [
     {
       path: '/',
-     name: 'home',
+      name: 'home',
       component: Home
     },
     {
-      path: '/posts/new',
-      component: NewPost
-    },
-  ]
-})
+      path: '/:pathMatch(.*)*',
+      redirect: '/'
+    }
+];
 
-export default router
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+
+  const routeExists = routes.some(route => route.path === to.path);
+  if (!routeExists) {
+    // document.title = 'Home'; //examples on how to set the chrome tab title
+    next({ path: '/', replace: true });
+  } else {
+    // document.title = to.meta.title as string || 'The Portfolio';
+    next();
+  }
+});
+
+export default router;
