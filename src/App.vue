922d08a25409cc63d4b1d72b59c790206a990307
Tony Duran <tony-duran@outlook.com>
2024-06-19 21:00:23 -0500

    initial setup for light/dark mode button

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
-<script lang="ts" setup>
-import NavBar from '@/components/NavBar.vue';
+<script setup>
+import { ref, onMounted } from 'vue';
+import NavBar from "@/components/NavBar.vue"
+import { useStorage } from "@vueuse/core";
 
+const theme = useStorage("current-theme", "dark");
+const isDarkMode = ref(true);
+
+const loadTheme = () => {
+  const theme = isDarkMode.value ? 'dark-mode' : 'light-mode';
+  removeCurrentTheme();
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = `/${theme}.css`;  // Adjusted to load from the public directory
+  link.id = 'theme-style';
+  document.head.appendChild(link);
+};
+
+const removeCurrentTheme = () => {
+  const existingLink = document.getElementById('theme-style');
+  if (existingLink) {
+    document.head.removeChild(existingLink);
+  }
+};
+
+const toggleTheme = () => {
+  isDarkMode.value = !isDarkMode.value;
+  loadTheme();
+};
+
+onMounted(() => {
+  loadTheme();
+});
 </script>
+
 <template>
-  <div class="section">
-    <div class="container">
-      <NavBar />
-      <RouterView />
-    </div>
+  <div id="app">
+    <label>
+      <input type="checkbox" v-model="isDarkMode" @change="toggleTheme" />
+      Toggle Light/Dark Mode
+    </label>
+    <NavBar  :theme="theme" @toggle-theme="toggleTheme" />
+    <router-view />
   </div>
 </template>
-
-<style>
-@import "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css";
-@import "highlight.js/styles/atom-one-dark.css";
-</style>
